6ee47bfc1323859ff5293c6a63116cafef0a8ad3
unknown <lindsay.kelly15@gmail.com>
2022-03-21 18:52:28 -0400

    accounts-router complete, 16/24 test passing

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,66 @@
-const router = require('express').Router()
+// Imports
+const router = require('express').Router();
+const Accounts = require('./accounts-model');
+const { checkAccountPayload, checkAccountNameUnique, checkAccountId } = require('./accounts-middleware');
 
+// Endpoints
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.getAll()
+    .then(accounts => {
+      res.json(accounts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    })
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', checkAccountId, (req, res, next) => {
+  const { id } = req.params;
+    Accounts.getById(id)
+      .then(account => {
+        res.status(200).json(account);
+      })
+      .catch(err => {
+        res.status(400).json({ message: err.message });
+      })
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
+  Accounts.create(req.body)
+    .then(newAccount => {
+      res.status(201).json(newAccount);
+    })
+    .catch(err => {
+      res.status(400).json({ message: err.message });
+    })
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', checkAccountId, (req, res, next) => {
+  Accounts.updateById(req.params.id, req.body)
+    .then(updatedAccount => {
+      res.status(200).json(updatedAccount);
+    })
+    .catch(err => {
+      res.status(400).json({ message: err.message});
+    })
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', checkAccountId, (req, res, next) => {
+  Accounts.deleteById(req.params.id)
+    .then(deletedAccount => {
+      res.status(200).json(deletedAccount);
+    })
+    .catch(err => {
+      res.status(400).json({ message: err.message });
+    })
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    customError: 'something went wrong',
+    message: err.message,
+    stack: err.stack
+  })
 })
 
 module.exports = router;
