736df3da3e3e4b528710c23ffc7ec3eae0373cd8
unknown <lindsay.kelly15@gmail.com>
2022-03-21 16:30:47 -0400

    server up

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
+// Server
 const express = require("express");
-
 const server = express();
-
 server.use(express.json());
 
+// Router
+const accountsRouter = require('./accounts/accounts-router');
+server.use('/api/accounts/', accountsRouter);
+
+// Export
 module.exports = server;
